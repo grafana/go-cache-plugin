30d18b1e5679f58a2006a7df7144f04f72da1ed2
grambbledook <denis.vodopianov@grafana.com>
2026-02-13 10:25:50 +0100

    update collectors.go to properly handle missing auth config

--- lib/otel/collectors.go
+++ lib/otel/collectors.go
@@ -17,6 +17,13 @@ import (
 
 var tp *trace.TracerProvider
 
+const (
+	OtelEndpoint         = "OTEL_EXPORTER_OTLP_ENDPOINT"
+	OtelEndpointTraces   = "OTEL_EXPORTER_OTLP_TRACES_ENDPOINT"
+	OtelHeaders          = "OTEL_EXPORTER_OTLP_HEADERS"
+	OtelAuthHeaderPrefix = "Authentication=Basic"
+)
+
 func SetupLoggingProvider(ctx context.Context, file string) (func(context.Context) error, error) {
 	f, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
@@ -30,13 +37,32 @@ func SetupLoggingProvider(ctx context.Context, file string) (func(context.Contex
 	return shutdownHook, nil
 }
 
+func checkTracingConfigured() error {
+	if os.Getenv(OtelEndpoint) == "" && os.Getenv(OtelEndpointTraces) == "" {
+		return fmt.Errorf("%s or %s env variable is not set", OtelEndpoint, OtelEndpointTraces)
+	}
+
+	headers := os.Getenv(OtelHeaders)
+	if !strings.HasPrefix(headers, OtelAuthHeaderPrefix) {
+		return fmt.Errorf("%s does not contain Authentication header", OtelHeaders)
+	}
+
+	if strings.TrimSpace(strings.TrimPrefix(headers, OtelAuthHeaderPrefix)) == "" {
+		return fmt.Errorf("Authentication header must be set with format 'Authorization=Basic ***'")
+	}
+	return nil
+}
 func SetupOtelTraceProvider(ctx context.Context) (func(context.Context) error, error) {
-	if os.Getenv("OTEL_EXPORTER_OTLP_ENDPOINT") == "" && os.Getenv("OTEL_EXPORTER_OTLP_TRACES_ENDPOINT") == "" {
-		return nil, fmt.Errorf("OTEL_EXPORTER_OTLP_ENDPOINT or OTEL_EXPORTER_OTLP_TRACES_ENDPOINT env variable is not set")
+
+	if err := checkTracingConfigured(); err != nil {
+		return nil, err
 	}
 
 	auth := os.Getenv("OTEL_EXPORTER_OTLP_HEADERS")
 	sep := strings.Index(auth, "=")
+	if sep < 0 {
+		return nil, fmt.Errorf("invalid format for OTEL_EXPORTER_OTLP_HEADERS")
+	}
 	name, value := auth[:sep], auth[sep+1:]
 
 	exporter, err := otlptracehttp.New(ctx,
